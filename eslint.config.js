4d15f0def9149aefc31bd2d033f94a4b5f433420
digitalveysel <hello@veysel.co>
2025-04-22 02:39:23 +0300

    change selector prefix

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,7 @@ module.exports = tseslint.config(
         "error",
         {
           type: "attribute",
-          prefix: "app",
+          prefix: "vd",
           style: "camelCase",
         },
       ],
@@ -26,7 +26,7 @@ module.exports = tseslint.config(
         "error",
         {
           type: "element",
-          prefix: "app",
+          prefix: "vd",
           style: "kebab-case",
         },
       ],

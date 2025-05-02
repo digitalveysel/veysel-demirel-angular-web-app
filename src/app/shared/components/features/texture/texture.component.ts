a5de749d2a95cc6b6e03353903b501becceb2215
digitalveysel <hello@veysel.co>
2025-05-02 18:52:56 +0300

    update texture component

--- src/app/shared/components/features/texture/texture.component.ts
+++ src/app/shared/components/features/texture/texture.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'vd-texture',
   template: `<span
-    class="pointer-events-none fixed inset-0 z-10 bg-[url(/images/texture.webp)] bg-size-[calc(--spacing(32))] bg-repeat opacity-[0.04] select-none"
+    class="pointer-events-none fixed inset-0 z-10 bg-[url(/images/texture.webp)] bg-size-[calc(--spacing(32))] bg-repeat opacity-[0.06] select-none"
   ></span>`,
 })
 export class TextureComponent {}

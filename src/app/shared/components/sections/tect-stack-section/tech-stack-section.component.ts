d69991506979fe34906c9c238132ab72649f5260
digitalveysel <hello@veysel.co>
2025-05-05 16:05:39 +0300

    update tech stack section component grid

--- src/app/shared/components/sections/tect-stack-section/tech-stack-section.component.ts
+++ src/app/shared/components/sections/tect-stack-section/tech-stack-section.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../../ui/icon/icon.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'vd-tech-stack-section',
-  imports: [IconComponent],
+  imports: [IconComponent, NgClass],
   template: `<section id="techStack" aria-label="Tech Stack Content" class="space-y-4 py-16">
     <h2 class="font-montserrat-alternates text-6 font-semibold xl:pl-4">Tech stack</h2>
-    <div
+    <ul
       class="grid grid-cols-2 divide-x divide-y border border-neutral-600 bg-neutral-800 md:grid-cols-4"
     >
-      @for (name of techIcons; track name) {
-        <div class="group flex aspect-square items-center justify-center border-neutral-600">
+      @for (name of techIcons; track name; let idx = $index) {
+        <li
+          class="group flex aspect-square items-center justify-center border-neutral-600"
+          [ngClass]="{ 'border-b-0': idx === 6, 'md:border-b-0': idx === 4 || idx === 5 }"
+        >
           <vd-icon
             name="{{ name }}"
             className="text-neutral-100 opacity-50 group-hover:opacity-100"
           />
-        </div>
+        </li>
       }
-    </div>
+    </ul>
   </section>`,
 })
 export class TechStackSectionComponent {

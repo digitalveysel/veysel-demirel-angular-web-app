556a0712f86b0af0ac0c8db16fb3a81ec821fa76
digitalveysel <hello@veysel.co>
2025-05-13 19:45:24 +0300

    create logo animation

--- src/app/components/ui/logo/logo.component.ts
+++ src/app/components/ui/logo/logo.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AnimationDirective } from '../../../directives/animation/animation.directive';
 
 @Component({
   selector: 'vd-logo',
-  imports: [RouterLink],
+  imports: [RouterLink, AnimationDirective],
   template: `<a
     routerLink=""
     fragment="hero"
     id="logo"
-    class="text-20px flex items-center gap-x-1 font-bold line-through"
+    class="text-20px flex items-center gap-x-1 font-semibold line-through"
     role="img"
     aria-label="Logo"
   >
-    <span class="text-neutral-200">v</span>
-    <span class="text-neutral-200">e</span>
-    <span class="text-neutral-200">y</span>
-    <span class="text-neutral-200">s</span>
-    <span class="text-neutral-200">e</span>
-    <span class="text-neutral-200">l</span>
-    <span>d</span>
-    <span>e</span>
-    <span>m</span>
-    <span>i</span>
-    <span>r</span>
-    <span>e</span>
-    <span>l</span>
+    @for (letter of letters; let index = $index; track index) {
+      <span
+        [class]="{ 'text-neutral-200': index < 6 }"
+        [vdAnimation]="{
+          keyframes: { y: [-10, 0], opacity: [0, 1] },
+          options: { delay: index * 0.1 },
+        }"
+      >
+        {{ letter }}
+      </span>
+    }
   </a>`,
 })
-export class LogoComponent {}
+export class LogoComponent {
+  letters: string[] = ['v', 'e', 'y', 's', 'e', 'l', 'd', 'e', 'm', 'i', 'r', 'e', 'l'];
+}

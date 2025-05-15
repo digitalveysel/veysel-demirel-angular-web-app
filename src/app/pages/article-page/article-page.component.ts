44a1bf3fca3c0f7e74f38d620bde25683c50c11a
digitalveysel <hello@veysel.co>
2025-05-15 03:14:05 +0300

    update article page component

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -6,85 +6,93 @@ import { IArticle, IBlockTypes, IRunTypes } from '../../core/models/article.mode
 import { AnimationDirective } from '../../directives/animation/animation.directive';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '../../core/services/article/article.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'vd-article-page',
-  imports: [ListComponent, TitleComponent, BlockquoteComponent, AnimationDirective],
+  imports: [ListComponent, TitleComponent, BlockquoteComponent, AnimationDirective, AsyncPipe],
   template: `
-    <article>
-      <vd-title vdLevel="h1" [vdText]="article.title" />
-      <div class="mb-12 space-y-9 border border-neutral-600 bg-neutral-800 p-6 lg:mb-16">
-        @for (section of article.sections; track section.id) {
-          <section class="space-y-6">
-            @for (block of section.blocks; track block.id) {
-              <div
-                class="first:mb-0"
-                [vdAnimation]="{
-                  type: 'inView',
-                  keyframes: {
-                    opacity: [0, 1],
-                  },
-                  options: { duration: 1 },
-                  callbackKeyframes: { opacity: 0 },
-                }"
-              >
-                @switch (block.type) {
-                  @case (blockTypes.TITLE) {
-                    <vd-title [vdLevel]="block.data.level" [vdText]="block.data.text" />
-                  }
-                  @case (blockTypes.PARAGRAPH) {
-                    <p class="text-neutral-200">
-                      @for (run of block.data.runs; track run.id) {
-                        @switch (run.type) {
-                          @case (runTypes.TEXT) {
-                            {{ run.text }}
-                          }
-                          @case (runTypes.LINK) {
-                            <a
-                              class="font-medium text-neutral-100 underline decoration-orange-500 decoration-2"
-                              [href]="run.href"
-                              [target]="run.target"
-                              [rel]="run.rel"
-                            >
+    @let article = $article | async;
+    @if (article) {
+      <article>
+        <vd-title vdLevel="h1" [vdText]="article.title" />
+        <div class="mb-12 space-y-9 border border-neutral-600 bg-neutral-800 p-6 lg:mb-16">
+          @for (section of article.sections; track section.id) {
+            <section class="space-y-6">
+              @for (block of section.blocks; track block.id) {
+                <div
+                  [class]="{ 'first:mb-0': block.type === blockTypes.TITLE }"
+                  [vdAnimation]="{
+                    type: 'inView',
+                    keyframes: {
+                      opacity: [0, 1],
+                    },
+                    options: { duration: 1 },
+                    callbackKeyframes: { opacity: 0 },
+                  }"
+                >
+                  @switch (block.type) {
+                    @case (blockTypes.TITLE) {
+                      <vd-title [vdLevel]="block.data.level" [vdText]="block.data.text" />
+                    }
+                    @case (blockTypes.PARAGRAPH) {
+                      <p class="text-neutral-200">
+                        @for (run of block.data.runs; track run.id) {
+                          @switch (run.type) {
+                            @case (runTypes.TEXT) {
                               {{ run.text }}
-                            </a>
+                            }
+                            @case (runTypes.LINK) {
+                              <a
+                                class="font-medium text-neutral-100 underline decoration-orange-500 decoration-2"
+                                [href]="run.href"
+                                [target]="run.target"
+                                [rel]="run.rel"
+                              >
+                                {{ run.text }}
+                              </a>
+                            }
                           }
                         }
-                      }
-                    </p>
+                      </p>
+                    }
+                    @case (blockTypes.BLOCKQUOTE) {
+                      <vd-blockquote
+                        [vdText]="block.data.text"
+                        [vdCiteText]="block.data.citeText"
+                      />
+                    }
+                    @case (blockTypes.LIST) {
+                      <vd-list
+                        vdListClass="space-y-2"
+                        vdListIcon="check-circle"
+                        vdListIconClass="text-orange-400"
+                        vdListIconSize="20"
+                        vdListItemClass="flex gap-x-2 text-14px"
+                        [vdList]="block.data.items"
+                      />
+                    }
+                    @case (blockTypes.IMAGE) {
+                      <img [src]="block.data.src" [alt]="block.data.alt" loading="lazy" />
+                    }
+                    @case (blockTypes.CODE) {
+                      <code> {{ block.data.source }}</code>
+                    }
                   }
-                  @case (blockTypes.BLOCKQUOTE) {
-                    <vd-blockquote [vdText]="block.data.text" [vdCiteText]="block.data.citeText" />
-                  }
-                  @case (blockTypes.LIST) {
-                    <vd-list
-                      vdListClass="space-y-2"
-                      vdListIcon="check-circle"
-                      vdListIconClass="text-orange-400"
-                      vdListIconSize="20"
-                      vdListItemClass="flex gap-x-2 text-14px"
-                      [vdList]="block.data.items"
-                    />
-                  }
-                  @case (blockTypes.IMAGE) {
-                    <img [src]="block.data.src" [alt]="block.data.alt" loading="lazy" />
-                  }
-                  @case (blockTypes.CODE) {
-                    <code> {{ block.data.source }}</code>
-                  }
-                }
-              </div>
-            }
-          </section>
-        }
-      </div>
-    </article>
+                </div>
+              }
+            </section>
+          }
+        </div>
+      </article>
+    }
   `,
 })
 export class ArticlePageComponent implements OnInit {
   blockTypes = IBlockTypes;
   runTypes = IRunTypes;
-  article!: IArticle;
+  $article!: Observable<IArticle>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -93,8 +101,6 @@ export class ArticlePageComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.paramMap.get('id') || '';
-    this.articleService.getById(id).subscribe((article) => {
-      this.article = article;
-    });
+    this.$article = this.articleService.getById(id);
   }
 }

034016f7e14145132f99130963205e482743b7e7
digitalveysel <hello@veysel.co>
2025-05-20 19:10:14 +0300

    update netlify ssr functions

--- netlify/functions/ssr.ts
+++ netlify/functions/ssr.ts
@@ -1,27 +1,35 @@
+import { AngularNodeAppEngine, writeResponseToNodeResponse } from '@angular/ssr/node';
 import express from 'express';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import serverless from 'serverless-http';
 import AppConfig from '../../src/server/app.config';
 import ContactController from '../../src/server/controllers/contact.controller';
 import { Routes } from '../../src/server/models/routes.model';
 
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(serverDistFolder, '../browser');
+
 const app = express();
+const angularApp = new AngularNodeAppEngine();
+
 const config = AppConfig.getInstance().values;
 const contactController = new ContactController(config);
 
 app.use(express.json());
 app.use(`${Routes.API}${Routes.CONTACT}`, contactController.getRouter());
-app.use('/**', async (req, res, next) => {
-  try {
-    const { AngularNodeAppEngine, writeResponseToNodeResponse } = await import('@angular/ssr/node');
-    const angularApp = new AngularNodeAppEngine();
-    const response = await angularApp.handle(req);
-    if (response) {
-      return writeResponseToNodeResponse(response, res);
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
+app.use(
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false,
+  }),
+);
+app.use('/**', (req, res, next) => {
+  angularApp
+    .handle(req)
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+    .catch(next);
 });
 
 export const handler = serverless(app);
